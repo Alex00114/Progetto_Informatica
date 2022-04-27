fb4d8b6c2c8dfcca37026353fd13b205f42137bb
Alex00114 <tag.alessandro@iisgalvanimi.edu.it>
2022-04-27 17:07:40 +0000

    .

--- progetto.py
+++ progetto.py
@@ -23,17 +23,50 @@ def registrazione():
 
 @app.route('/quiz', methods=['GET'])
 def quiz():
-    global nick, sesso
+    global nick, sesso, mappa_regione
     nick = request.args["Name"]
     sesso = request.args["Sex"]
-    i = random.randint(0, 19)
     regioni_name = list(regioni["DEN_REG"])
-    regione = regioni_name[i]
-    mappa_regione = regioni[regioni["DEN_REG"] == regione]
-    return render_template("quiz.html", text = regione)
+    i = random.randint(0, 19)
+    j = random.randint(0, 19)
+    k = random.randint(0, 19)
+    l = random.randint(0, 19)
+    opzioni = random.randint(0, 3)
+    if opzioni == 1:
+        regione = regioni_name[i]
+        opz2 = regioni_name[j]
+        opz3 = regioni_name[k]
+        opz4 = regioni_name[l]
+        mappa_regione = regioni[regioni["DEN_REG"] == regione]
+    elif opzioni == 2:
+        regione = regioni_name[j]
+        opz2 = regioni_name[i]
+        opz3 = regioni_name[l]
+        opz4 = regioni_name[k]
+        mappa_regione = regioni[regioni["DEN_REG"] == regione]
+    elif opzioni == 3:
+        regione = regioni_name[k]
+        opz2 = regioni_name[j]
+        opz3 = regioni_name[i]
+        opz4 = regioni_name[l]
+        mappa_regione = regioni[regioni["DEN_REG"] == regione]
+    else:
+        regione = regioni_name[l]
+        opz2 = regioni_name[k]
+        opz3 = regioni_name[j]
+        opz4 = regioni_name[i]
+        mappa_regione = regioni[regioni["DEN_REG"] == regione]
+    return render_template("quiz.html", opzione1 = regione, opzione2 = opz2, opzione3 = opz3, opzione4 = opz4)
     
+@app.route('/regione_png', methods=['GET'])
+def regione_png():
+    fig, ax = plt.subplots(figsize = (12,8))
 
-
+    mappa_regione.to_crs(epsg=3857).plot(ax=ax, edge_color = "k")
+    contextily.add_basemap(ax=ax)
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
 
 
 
