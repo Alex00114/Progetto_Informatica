691cae9e46679729e6a50daa05a15143a72fef4e
Goyco2 <goycochea.q@iisgalvanimi.edu.it>
2022-05-11 12:54:05 +0000

    .

--- ricerca.py
+++ ricerca.py
@@ -14,18 +14,17 @@ from folium.plugins import MousePosition
 
 
 regioniData= pd.read_csv('/workspace/Progetto_Informatica/static/csv/regioni - Foglio1.csv')
-ProvinceGeo = geopandas.read_file('/workspace/Progetto_Informatica/templates/georef-italy-provincia-millesime.geojson')
-ProvinceGeo.drop_duplicates(subset=["prov_name"])
-province_name = list(ProvinceGeo["prov_name"])
 coordinateReg = pd.read_csv('/workspace/Progetto_Informatica/static/csv/regioniCoord - Foglio1.csv')
+coordinateProv = pd. read_csv('/workspace/Progetto_Informatica/static/csv/coorProv - Foglio1.csv')
 Regioni = geopandas.read_file('/workspace/Progetto_Informatica/limits_IT_regions.geojson')
+Province = geopandas.read_file('/workspace/Progetto_Informatica/limits_IT_provinces.geojson')
 coordinateRegioniMerge = coordinateReg.merge(Regioni, how='inner', left_on='name', right_on='reg_name')
 coordinateRegData = coordinateReg.merge(regioniData, how='inner', left_on='name', right_on='Regione')
+coordinateProvinceMerge = coordinateProv.merge(Province, how='inner', left_on='Provincia', right_on='prov_name')
 
 @app.route('/', methods=['GET'])
 def mappaF():
   m = folium.Map(location=[41.2925, 12.5736], tiles="openstreetmap",zoom_start=6.3, min_zoom = 5)
-  
   
   for index, row in coordinateRegioniMerge.iterrows():
     folium.Marker([row["lat"], row["lon"]], popup=regioniData).add_to(m)
@@ -65,10 +64,45 @@ def png2():
     
     return render_template("mappaRichiesta.html")
 
+@app.route('/Province', methods=['GET'])
+def Province():
+  m = folium.Map(location=[41.2925, 12.5736], tiles="openstreetmap",zoom_start=6.3, min_zoom = 5)
+  for index, row in coordinateProvinceMerge.iterrows():
+    folium.Marker([row["lat"], row["lon"]]).add_to(m)
 
+  folium.GeoJson('/workspace/Progetto_Informatica/limits_IT_provinces.geojson', name="geojson").add_to(m)
+  folium.LayerControl().add_to(m)
+  m.save('templates/mappa.html')
+  return render_template('province.html')
 
+@app.route('/mappa', methods=['GET'])
+def png3():\
+    
+    return render_template("mappa.html")
 
+@app.route('/ricercaProv', methods=['GET'])
+def ricercaProv():
+  prov_lista = list(Province["prov_name"])
+  Provincia = request.args["CercaProv"]
+  provincia_richiesta = Province[Province["prov_name"].str.contains(Provincia)]
+  provincia_richiesta2 = coordinateProvinceMerge[coordinateProvinceMerge.reg_name.str.contains(Provincia)]
+  if Provincia in prov_lista:
+    latitudine = provincia_richiesta2["lat"]
+    longitudine = provincia_richiesta2["lon"]
+    m = folium.Map(location= [latitudine,longitudine], tiles="openstreetmap",zoom_start=9, min_zoom = 8)
+    folium.GeoJson(provincia_richiesta, name="geojson").add_to(m)
 
+    folium.LayerControl().add_to(m)
+    MousePosition().add_to(m)
+    m.save('templates/mappaRichiestaProv.html')
+    return render_template('cercaProv.html')
+  else:
+    return '<h1>ERRORE</h1>'
+
+@app.route('/mappaRichiestaProv', methods=['GET'])
+def png4():
+    
+    return render_template("mappaRichiestaProv.html")
 
 
 
