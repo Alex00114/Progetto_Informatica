54625b96cf06b141209eee58115920961fdf4097
Alex00114 <tag.alessandro@iisgalvanimi.edu.it>
2022-05-11 12:49:17 +0000

    .

--- progetto.py
+++ progetto.py
@@ -68,7 +68,7 @@ def login():
             email = request.form.get("Email")
             for i, d in dati.iterrows():
                 if email == d["email"] and password == d["password"]:  
-                    return 'h1>Login</h1>'<
+                    return '<h1>Login</h1>'
 
             return '<h1>Errore</h1>'
 
@@ -170,6 +170,101 @@ def quiz_facile():
         return redirect(url_for("risultato_facile"))
 
     return render_template("quiz_facile.html", opzione1 = opz1, opzione2 = opz2, opzione3 = opz3, opzione4 = opz4, score = punteggio, text = testo)
+
+@app.route('/quiz_difficile', methods=['GET'])
+def quiz_difficile():
+    global mappa_quiz, risposta, volte, punteggio, corretta, valore_max, regioni
+    regioni_province = random.randint(0,1)
+
+    if volte >=0:
+        volte = volte + 1
+
+    if volte >=2:
+        risposta = request.args["Scelta"]
+        if risposta in corretta:
+            punteggio = punteggio + 1
+
+    if regioni_province == 0:
+        testo = "Indovina la Regione!"
+        unico = random.sample(range(0, 20), 4)
+        i = unico[0]
+        j = unico[1]
+        k = unico[2]
+        l = unico[3]
+
+        opzioni = random.randint(0, 3)
+        if opzioni == 0:
+            opz1 = regioni_name[i]
+            opz2 = regioni_name[j]
+            opz3 = regioni_name[k]
+            opz4 = regioni_name[l]
+            mappa_quiz = regioni[regioni["DEN_REG"] == opz1]
+            corretta = opz1
+        elif opzioni == 1:
+            opz1 = regioni_name[j]
+            opz2 = regioni_name[i]
+            opz3 = regioni_name[l]
+            opz4 = regioni_name[k]
+            mappa_quiz = regioni[regioni["DEN_REG"] == opz2]
+            corretta = opz2
+        elif opzioni == 2:
+            opz1 = regioni_name[k]
+            opz2 = regioni_name[j]
+            opz3 = regioni_name[i]
+            opz4 = regioni_name[l]
+            mappa_quiz = regioni[regioni["DEN_REG"] == opz3]
+            corretta = opz3
+        else:
+            opz1 = regioni_name[l]
+            opz2 = regioni_name[k]
+            opz3 = regioni_name[j]
+            opz4 = regioni_name[i]
+            mappa_quiz = regioni[regioni["DEN_REG"] == opz4]
+            corretta = opz4
+    else:
+        unico = random.sample(range(0, valore_max), 4)
+        i = unico[0]
+        j = unico[1]
+        k = unico[2]
+        l = unico[3]
+
+        opzioni = random.randint(0, 3)
+        if opzioni == 0:
+            opz1 = province_name[i]
+            opz2 = province_name[j]
+            opz3 = province_name[k]
+            opz4 = province_name[l]
+            mappa_quiz = province[province["DEN_PROV"] == opz1]
+            corretta = opz1
+        elif opzioni == 1:
+            opz1 = province_name[j]
+            opz2 = province_name[i]
+            opz3 = province_name[l]
+            opz4 = province_name[k]
+            mappa_quiz = province[province["DEN_PROV"] == opz2]
+            corretta = opz2
+        elif opzioni == 2:
+            opz1 = province_name[k]
+            opz2 = province_name[j]
+            opz3 = province_name[i]
+            opz4 = province_name[l]
+            mappa_quiz = province[province["DEN_PROV"] == opz3]
+            corretta = opz3
+        else:
+            opz1 = province_name[l]
+            opz2 = province_name[k]
+            opz3 = province_name[j]
+            opz4 = province_name[i]
+            mappa_quiz = province[province["DEN_PROV"] == opz4]
+            corretta = opz4
+        
+        reg_provincia = regioni[regioni.contains(mappa_quiz.geometry.squeeze())]
+        reg_provincia = regioni_name[regioni_name == str(reg_provincia["DEN_REG"])]
+        testo =  "Indovina la Provincia!"
+    if volte >=11:
+        return redirect(url_for("risultato_difficile"))
+
+    return render_template("quiz_difficile.html", opzione1 = opz1, opzione2 = opz2, opzione3 = opz3, opzione4 = opz4, score = punteggio, text = testo)
     
 @app.route('/regione_png', methods=['GET'])
 def regione_png():
