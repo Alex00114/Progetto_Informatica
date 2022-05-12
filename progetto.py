862c36d9b53909ccb1c6cd13b61d9997bd8b2c26
Alex00114 <tag.alessandro@iisgalvanimi.edu.it>
2022-05-12 09:50:51 +0000

    .

--- progetto.py
+++ progetto.py
@@ -24,13 +24,13 @@ valore_max = len(province_name)
 dati = pd.read_csv("/workspace/Progetto_Informatica/static/csv/dati.csv")
 
 volte = 0
+domanda = 1
 punteggio = 0
 
 
 @app.route('/', methods=['GET'])
 def home():
-    tabella = pd.read_csv("/workspace/Progetto_Informatica/static/csv/regioniCoord - Foglio1.csv")
-    return render_template('home.html', table = tabella.to_html())
+    return render_template('home.html')
 
 # @app.route('/accedi', methods=['GET'])
 # def accedi():
@@ -78,7 +78,7 @@ def difficolta():
 
 @app.route('/quiz_facile', methods=['GET'])
 def quiz_facile():
-    global mappa_quiz, risposta, volte, punteggio, corretta, valore_max, regioni
+    global mappa_quiz, risposta, volte, punteggio, corretta, valore_max, regioni, domanda
     regioni_province = random.randint(0,1)
 
     if volte >=0:
@@ -163,17 +163,18 @@ def quiz_facile():
             mappa_quiz = province[province["DEN_PROV"] == opz4]
             corretta = opz4
         
+        domanda = domanda + 1
         reg_provincia = regioni[regioni.contains(mappa_quiz.geometry.squeeze())]
         reg_provincia = regioni_name[regioni_name == str(reg_provincia["DEN_REG"])]
         testo =  "Indovina la Provincia appartenente alla Regione " + reg_provincia
     if volte >=11:
         return redirect(url_for("risultato_facile"))
 
-    return render_template("quiz_facile.html", opzione1 = opz1, opzione2 = opz2, opzione3 = opz3, opzione4 = opz4, score = punteggio, text = testo)
+    return render_template("quiz_facile.html", opzione1 = opz1, opzione2 = opz2, opzione3 = opz3, opzione4 = opz4, score = punteggio, text = testo, question = domanda)
 
 @app.route('/quiz_difficile', methods=['GET'])
 def quiz_difficile():
-    global mappa_quiz, risposta, volte, punteggio, corretta, valore_max, regioni
+    global mappa_quiz, risposta, volte, punteggio, corretta, valore_max, regioni, domanda
     regioni_province = random.randint(0,1)
 
     if volte >=0:
@@ -258,14 +259,105 @@ def quiz_difficile():
             mappa_quiz = province[province["DEN_PROV"] == opz4]
             corretta = opz4
         
-        reg_provincia = regioni[regioni.contains(mappa_quiz.geometry.squeeze())]
-        reg_provincia = regioni_name[regioni_name == str(reg_provincia["DEN_REG"])]
+        domanda = domanda + 1
         testo =  "Indovina la Provincia!"
     if volte >=11:
         return redirect(url_for("risultato_difficile"))
 
-    return render_template("quiz_difficile.html", opzione1 = opz1, opzione2 = opz2, opzione3 = opz3, opzione4 = opz4, score = punteggio, text = testo)
+    return render_template("quiz_difficile.html", opzione1 = opz1, opzione2 = opz2, opzione3 = opz3, opzione4 = opz4, score = punteggio, text = testo, question = domanda)
     
+@app.route('/quiz_difficile2', methods=['GET'])
+def quiz_difficile2():
+    global mappa_quiz, risposta, volte, punteggio, corretta, valore_max, regioni, domanda
+    regioni_province = random.randint(0,1)
+
+    if volte >=0:
+        volte = volte + 1
+
+    if volte >=2:
+            punteggio = punteggio + 0
+
+    if regioni_province == 0:
+        testo = "Indovina la Regione!"
+        unico = random.sample(range(0, 20), 4)
+        i = unico[0]
+        j = unico[1]
+        k = unico[2]
+        l = unico[3]
+
+        opzioni = random.randint(0, 3)
+        if opzioni == 0:
+            opz1 = regioni_name[i]
+            opz2 = regioni_name[j]
+            opz3 = regioni_name[k]
+            opz4 = regioni_name[l]
+            mappa_quiz = regioni[regioni["DEN_REG"] == opz1]
+            corretta = opz1
+        elif opzioni == 1:
+            opz1 = regioni_name[j]
+            opz2 = regioni_name[i]
+            opz3 = regioni_name[l]
+            opz4 = regioni_name[k]
+            mappa_quiz = regioni[regioni["DEN_REG"] == opz2]
+            corretta = opz2
+        elif opzioni == 2:
+            opz1 = regioni_name[k]
+            opz2 = regioni_name[j]
+            opz3 = regioni_name[i]
+            opz4 = regioni_name[l]
+            mappa_quiz = regioni[regioni["DEN_REG"] == opz3]
+            corretta = opz3
+        else:
+            opz1 = regioni_name[l]
+            opz2 = regioni_name[k]
+            opz3 = regioni_name[j]
+            opz4 = regioni_name[i]
+            mappa_quiz = regioni[regioni["DEN_REG"] == opz4]
+            corretta = opz4
+    else:
+        unico = random.sample(range(0, valore_max), 4)
+        i = unico[0]
+        j = unico[1]
+        k = unico[2]
+        l = unico[3]
+
+        opzioni = random.randint(0, 3)
+        if opzioni == 0:
+            opz1 = province_name[i]
+            opz2 = province_name[j]
+            opz3 = province_name[k]
+            opz4 = province_name[l]
+            mappa_quiz = province[province["DEN_PROV"] == opz1]
+            corretta = opz1
+        elif opzioni == 1:
+            opz1 = province_name[j]
+            opz2 = province_name[i]
+            opz3 = province_name[l]
+            opz4 = province_name[k]
+            mappa_quiz = province[province["DEN_PROV"] == opz2]
+            corretta = opz2
+        elif opzioni == 2:
+            opz1 = province_name[k]
+            opz2 = province_name[j]
+            opz3 = province_name[i]
+            opz4 = province_name[l]
+            mappa_quiz = province[province["DEN_PROV"] == opz3]
+            corretta = opz3
+        else:
+            opz1 = province_name[l]
+            opz2 = province_name[k]
+            opz3 = province_name[j]
+            opz4 = province_name[i]
+            mappa_quiz = province[province["DEN_PROV"] == opz4]
+            corretta = opz4
+        
+        domanda = domanda + 1
+        testo =  "Indovina la Provincia!"
+    if volte >=11:
+        return redirect(url_for("risultato_difficile"))
+
+    return render_template("quiz_difficile.html", opzione1 = opz1, opzione2 = opz2, opzione3 = opz3, opzione4 = opz4, score = punteggio, text = testo, question = domanda)
+
 @app.route('/regione_png', methods=['GET'])
 def regione_png():
     fig, ax = plt.subplots(figsize = (12,8))
